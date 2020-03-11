265440f0a4c6f76da66c52a08ac7958f94837095
Jack Quigley <jackaquigley@gmail.com>
2020-03-11 21:21:34 +0000

    got the lowest value from eriks array

--- exercise_b_users.rb
+++ exercise_b_users.rb
@@ -67,7 +67,13 @@ p users["Erik"][:home_town]
 p users["Erik"][:lottery_numbers]
 
 # 4. Get the species of Avril's pet Monty
+
+# p users["Avril"][:pets][:species] #couldn't get this one to work
+
 # 5. Get the smallest of Erik's lottery numbers
+
+p users["Erik"][:lottery_numbers].min()
+
 # 6. Return an array of Avril's lottery numbers that are even
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
 # 8. Change Erik's hometown to Edinburgh
