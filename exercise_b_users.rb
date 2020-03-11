f02e95b51c95d72155d0dc95e81911321bc8982d
Jack Quigley <jackaquigley@gmail.com>
2020-03-11 21:23:01 +0000

    added to eriks array

--- exercise_b_users.rb
+++ exercise_b_users.rb
@@ -75,7 +75,12 @@ p users["Erik"][:lottery_numbers]
 p users["Erik"][:lottery_numbers].min()
 
 # 6. Return an array of Avril's lottery numbers that are even
+
+
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
+
+p users["Erik"][:lottery_numbers].push(7)
+
 # 8. Change Erik's hometown to Edinburgh
 # 9. Add a pet dog to Erik called "Fluffy"
 # 10. Add another person to the users hash
